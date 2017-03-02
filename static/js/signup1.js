fc91e17b1745412149f9a1053cf0af60d250b9d6
Shaun <seyyes2000@hotmail.com>
2017-03-02 10:37:09 -0500

    update files; try to get signup to work

---
+++ static/js/signup1.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $.ajaxSetup({
+        headers: {'Authorization': "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=="}
+    })
+    $('#btnSignUp').click(function(){
+        $.ajax({
+            url: '/signUp',
+            prepare: function (query, settings) {
+                settings.url = settings.url.replace('%QUERY', query);
+                settings.headers = {
+                    'appid': APP.APP_ID,
+                    'token': localStorageService.get('app-token'),
+                    'scope': 'app',
+                    'lang': 'en'
+                };
+            },
+            datatype: "application/json; charset=utf-8",
+            contentType: "text/plain",
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
+});
